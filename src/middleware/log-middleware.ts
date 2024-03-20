2317e48faef906c3a2c529ad671e5c0b937f2a26
restuadil <restuadil09@gmail.com>
2024-03-20 13:33:27 +0700

    adding log-middleware

---
+++ src/middleware/log-middleware.ts
@@ -0,0 +1,7 @@
+import { NextFunction, Request, Response } from "express";
+import { logger } from "../utils/logger";
+
+export const logMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    logger.info(`${req.method} ${req.url}`);
+    next();
+}
